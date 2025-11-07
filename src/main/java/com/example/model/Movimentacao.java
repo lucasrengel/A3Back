88c88caf46408e6982a2cfcededc70993ef87eb0
marcosalexper <marcosalexper@hotmail.com>
2025-11-07 14:58:52 -0300

    feat: model movimentacao

---
+++ src/main/java/com/example/model/Movimentacao.java
@@ -0,0 +1,53 @@
+package com.example.model;
+
+import java.io.Serializable;
+
+public class Movimentacao implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private long id;
+    private long produtoId;
+    private String data;
+    private int quantidade;
+    private String tipo;
+
+    public Movimentacao() {}
+
+    public Movimentacao(long id, long produtoId, String data, int quantidade, String tipo) {
+        this.id = id;
+        this.produtoId = produtoId;
+        this.data = data;
+        this.quantidade = quantidade;
+        this.tipo = tipo;
+    }
+
+    public Movimentacao(long produtoId, String data, int quantidade, String tipo) {
+        this.produtoId = produtoId;
+        this.data = data;
+        this.quantidade = quantidade;
+        this.tipo = tipo;
+    }
+
+    public Movimentacao(long produtoId, int quantidade, String tipo) {
+        this.produtoId = produtoId;
+        this.quantidade = quantidade;
+        this.tipo = tipo;
+    }
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+    public long getProdutoId() { return produtoId; }
+    public void setProdutoId(long produtoId) { this.produtoId = produtoId; }
+    public String getData() { return data; }
+    public void setData(String data) { this.data = data; }
+    public int getQuantidade() { return quantidade; }
+    public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+
+    @Override
+    public String toString() {
+        return "Movimentacao{id=" + id + ", produtoId=" + produtoId + ", data='" + data + 
+               "', quantidade=" + quantidade + ", tipo='" + tipo + "'}";
+    }
+}
