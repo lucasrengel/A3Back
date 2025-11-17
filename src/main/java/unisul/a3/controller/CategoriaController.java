8f8197e0cf853dda6f6794a0c1e590566d5f70e7
isaluch <Isadoraluch@gmail.com>
2025-11-17 09:26:19 -0300

    feat: add categoria controller and listar function

---
+++ src/main/java/unisul/a3/controller/CategoriaController.java
@@ -0,0 +1,23 @@
+package unisul.a3.controller;
+
+import unisul.a3.model.Categoria;
+import unisul.a3.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/categorias")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaService service;
+
+    @GetMapping
+    public List<Categoria> listar() {
+        return service.listar();
+    }
+}
