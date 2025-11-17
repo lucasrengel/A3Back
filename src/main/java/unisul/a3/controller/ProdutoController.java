65893cc1d02015222a9a1e6d799040ab3e63a0c2
isaluch <Isadoraluch@gmail.com>
2025-11-17 09:31:58 -0300

    feat: add produto controller and get function

---
+++ src/main/java/unisul/a3/controller/ProdutoController.java
@@ -0,0 +1,23 @@
+package unisul.a3.controller;
+
+import unisul.a3.model.Produto;
+import unisul.a3.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService service;
+
+    @GetMapping
+    public List<Produto> listar() {
+        return service.listar();
+    }
+}
