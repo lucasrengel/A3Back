0c2f2131231be6742f5916f1ac591cc9a67a59ca
isaluch <Isadoraluch@gmail.com>
2025-11-17 09:30:53 -0300

    feat: categoria controller delete function

--- src/main/java/unisul/a3/controller/CategoriaController.java
+++ src/main/java/unisul/a3/controller/CategoriaController.java
@@ -50,4 +50,14 @@ public class CategoriaController {
             return ResponseEntity.badRequest().body(Map.of("success", false, "error", e.getMessage()));
         }
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> remover(@PathVariable long id) {
+        try {
+            service.remover(id);
+            return ResponseEntity.ok(Map.of("success", true, "message", "Categoria removida com sucesso!"));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(Map.of("success", false, "error", e.getMessage()));
+        }
+    }
 }
