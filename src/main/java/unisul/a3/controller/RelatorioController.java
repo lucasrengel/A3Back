d464e75067a3b147734312adc057e3c874aaf72e
PedroEmanueL51027 <Brcpedroso5102@gmail.com>
2025-11-18 17:11:16 -0300

    feat: relatorio controller

---
+++ src/main/java/unisul/a3/controller/RelatorioController.java
@@ -0,0 +1,21 @@
+package unisul.a3.controller;
+
+import unisul.a3.model.*;
+import unisul.a3.service.RelatorioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/relatorios")
+public class RelatorioController {
+
+    @Autowired
+    private RelatorioService service;
+
+    @GetMapping("/lista-precos")
+    public RelatorioListaPreco listaPrecos() {
+        return service.getListaPreco();
+    }
+}
